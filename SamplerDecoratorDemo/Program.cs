e5b765b7acd652a31350fe6c3e96fc9455fae86f
RED-GATE\Jack.Read <jack.read@red-gate.com>
2022-11-04 12:29:31 +0000

    Write demo program

--- SamplerDecoratorDemo/Program.cs
+++ SamplerDecoratorDemo/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace SamplerDecoratorDemo
+﻿using System;
+using SamplerDecoratorDemo.Component;
+using SamplerDecoratorDemo.Decorator;
+
+namespace SamplerDecoratorDemo
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var topQuerySampler = new TopQuerySampler();
+            topQuerySampler.Sample();
+            Break();
+
+            var cumulative = new CumulativeSqlResultsSampler(topQuerySampler);
+            cumulative.Sample();
+            Break();
+
+            var tempTable = new TempTableCumulativeSqlResultsSampler(topQuerySampler);
+            tempTable.Sample();
+            Break();
+
+            Console.ReadLine();
+        }
+
+        private static void Break()
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine();
         }
     }
 }
